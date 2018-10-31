5c3c575c43a58d1e17c1bac6745c03d9d0ef4276
vinutha.garimella <vinutha.garimella@hotelsoft.com>
2018-10-31 15:09:29 +0530

    testing.....

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/vinutha96/docker-demo.git'){  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/Vinutha96/docker-demo.git'){  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
